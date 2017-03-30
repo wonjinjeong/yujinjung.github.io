e658af7a448aa5f8ead82adb7ef198d459565528
ujinjung <ujin.jung9@gmail.com>
2017-03-30 13:28:27 +0900

    first commit

---
+++ assets/js/_main.js
@@ -0,0 +1,63 @@
+/*! Plugin options and other jQuery stuff */
+
+// dl-menu options
+$(function() {
+  $( '#dl-menu' ).dlmenu({
+    animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
+  });
+});
+
+// FitVids options
+$(function() {
+  $("article").fitVids();
+});
+
+$(".close-menu").click(function () {
+  $(".menu").toggleClass("disabled");
+  $(".links").toggleClass("enabled");
+});
+
+$(".about").click(function () {
+  $("#about").css('display','block');
+});
+
+$(".close-about").click(function () {
+  $("#about").css('display','');
+});
+
+// Add lightbox class to all image links
+$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+// Magnific-Popup options
+$(document).ready(function() {
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 300, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open. 
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade'
+  });
+});
+
+// navigation
+$(function() {
+	$('.toggle').click(function() {
+		$('.nav-item').toggleClass('slide-out');
+		$('.hamburger-1').toggleClass('cross-right');
+		$('.hamburger-2').toggleClass('cross-hide');
+		$('.hamburger-3').toggleClass('cross-left');
+		$('.wobble').addClass('ripple');
+		setTimeout(function(){
+			$('.wobble').removeClass('ripple');
+		}, 1000);
+	});
+});
